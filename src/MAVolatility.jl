aadd41466020991d51312f920b9f1dfc2977452b
Kevin Atteson <kevin@atteson.com>
2019-01-05 15:45:18 -0500

    running

--- src/MAVolatility.jl
+++ src/MAVolatility.jl
@@ -20,9 +20,9 @@ function Base.rand( model::MAVolatilityModel{F}, n::Int ) where {F}
     N = 2^Int(ceil(log2(m)))
     epsilon = randn( m )
     delta = rand( n )
-    epsilon2 = Complex[epsilon; zeros(N - m)]
-    a = Complex[[model.f(i) for i in 1:n]; zeros(N - n)]
+    epsilon2 = Complex{Float64}[epsilon; zeros(N - m)]
+    a = Complex{Float64}[[model.f(i) for i in 1:n]; zeros(N - n)]
     convolve!( epsilon2, a, a )
-    x = exp.(cumsum( a[1:n] )) .* delta
+    x = exp.(cumsum( real.(a[1:n]) )) .* delta
     return (epsilon, x)
 end
